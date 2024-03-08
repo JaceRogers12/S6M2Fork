028f38a7d2bc40db5568424a66d26de5dfee9b5f
JaceRogers12 <jacerogers12@gmail.com>
2024-03-07 19:29:40 -0700

    probably finished project, started stretch

--- frontend/components/SearchBar/SearchBar.js
+++ frontend/components/SearchBar/SearchBar.js
@@ -1,12 +1,13 @@
 // You do not need to change any code in this file for MVP
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const {search, findSearch} = props
   return (
     <div className="search-bar-wrapper">
       <div className="social">📷</div>
       <form className="search-form">
-        <input type="text" placeholder="Search for a post..." />
+        <input type="text" placeholder="Search for a post..." onChange= {(event) => findSearch(event.target.value)}/>
       </form>
       <div className="social-wrapper">
         <div className="social">🫶</div>
